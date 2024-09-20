6217d8733a882a8929cef9c9ef37ea8df2edb46e
danielbrodd <77890040+danielbrodd@users.noreply.github.com>
2024-09-20 09:42:00 +0000

    validate email and country done. halfway with validate zip

--- main.js
+++ main.js
@@ -5,78 +5,91 @@ const pwd = document.querySelector("#pwd");
 const confpwd = document.querySelector("#cnfPwd");
 const error = document.querySelector(".error");
 
+const validCountries = ["USA","SWEDEN", "NIGERIA"];
 
 inputs.forEach(element => {
-    
+    console.log(element.name)
     element.addEventListener('focusout', () => {
-        let msg = customValidator(element)
-        console.log(msg)
-        error.innerHTML = msg
-        if (element.id = "cnfPwd") {
-            if (pwd != cnfPwd) {
-                msg = "Passwords do not match";
-            }
+        var validityState = element.validity
+        console.log(validityState)
+
+        var name = element.name
+        
+
+        
+
+        switch (name) {
+            case "email":
+                console.log(`this is: ${name}`);
+                if (validityState.valueMissing) {
+                    console.log("missing")
+                } else if (validityState.typeMismatch) {
+                    console.log("not an email")
+                } else if (validityState.valid) {
+                    console.log("valid")
+                } else {
+                    console.log("unkown error")
+                }
+                break;
+            case "country":
+                console.log(`this is: ${name}`);
+
+                if(validityState.valueMissing) {
+                    console.log("missing");
+                } else if (!validCountries.includes(element.value.toUpperCase())){
+                    console.log("not a country")
+                } else {
+                    console.log("Valid")
+                }
+                break;
+            case "zipCode":
+                console.log(`this is: ${name}`);
+                let zip = element.value;
+                if (zip.match("[a-zA-z+]")) {
+                    console.log("not a number")
+                } else {
+                    let zipNumber = parseInt(zip)
+                    console.log(typeof zipNumber);
+                }
+               
+                break;
+            case "password":
+                console.log(`this is: ${name}`);
+                break;
+            case "confirmPassowrd":
+                console.log(`this is: ${name}`);
+                break;
+        
+            default:
+                console.log("def")
+                break;
         }
+       
     });
 });
 
-zip.addEventListener('focusout', (e)=>{
-    msg = customValidator(zip);
-
-})
-
-email.addEventListener('focusout', (e)=>{
-
-    msg = customValidator(email)
 
 
-    const validityState = email.validity
 
-    switch(true) {
-        case validityState.valueMissing:
-            email.setCustomValidity("You must enter something")
-            break;
-        case validityState.typeMismatch:
-            email.setCustomValidity("And it has to be an email. Didn't I say that?")
-            break;
-        default:
-            console.log("Valid")
-    }
-
-    /* if (email.validity.typeMismatch) {
-        email.setCustomValidity("and it has to be an email");
-        
-    }else if (email.validity.valueMissing){
-        email.setCustomValidity("you must enter something")
-    }
-    else if (email.checkValidity()){
-        console.log("valid")
-    } else {
-        console.log("unknown")
-    }
-    email.reportValidity() */
-})
-
-
-function customValidator(target) {
-    validityState = target.validity;
-    labelText = target.labels[0].textContent;
-    var errorMsg = ""
-    switch(true){
-        case validityState.typeMismatch:
-            errorMsg = `you must enter: ${labelText}`;
+// function customValidator(target) {
+//     validityState = target.validity;
+//     labelText = target.labels[0].textContent;
+//     var errorMsg = ""
+//     switch(true){
+//         case validityState.typeMismatch:
+//             errorMsg = `you must enter: ${labelText}`;
             
-            break;
-        case validityState.valueMissing:
-            errorMsg = 'cannot be empty'
-            break;
-        case validityState.tooShort:
-            errorMsg = "too short";
-            break
-        default:
-            errorMsg = "";
-            break;
-    }
+//             break;
+//         case validityState.valueMissing:
+//             errorMsg = 'cannot be empty'
+//             break;
+//         case validityState.tooShort:
+//             errorMsg = "too short";
+//             break
+//         default:
+//             errorMsg = "";
+//             break;
+//     }
 
-    return errorMsg;
-}
+//     return errorMsg;
+// }
