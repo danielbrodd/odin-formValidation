c0eaaaf4f92981c2d6774c9bc15592077d93b110
danielbrodd <77890040+danielbrodd@users.noreply.github.com>
2024-09-20 12:01:02 +0000

    Continue refinning validity switch

--- main.js
+++ main.js
@@ -4,92 +4,76 @@ const inputs = document.querySelectorAll("input");
 const pwd = document.querySelector("#pwd");
 const confpwd = document.querySelector("#cnfPwd");
 const error = document.querySelector(".error");
+const validSubmission = false
 
 const validCountries = ["USA","SWEDEN", "NIGERIA"];
-
 inputs.forEach(element => {
-    console.log(element.name)
     element.addEventListener('focusout', () => {
-        var validityState = element.validity
-        console.log(validityState)
-
-        var name = element.name
+    validationSwitch(element);
         
-
-        
-
-        switch (name) {
-            case "email":
-                console.log(`this is: ${name}`);
-                if (validityState.valueMissing) {
-                    console.log("missing")
-                } else if (validityState.typeMismatch) {
-                    console.log("not an email")
-                } else if (validityState.valid) {
-                    console.log("valid")
-                } else {
-                    console.log("unkown error")
-                }
-                break;
-            case "country":
-                console.log(`this is: ${name}`);
-
-                if(validityState.valueMissing) {
-                    console.log("missing");
-                } else if (!validCountries.includes(element.value.toUpperCase())){
-                    console.log("not a country")
-                } else {
-                    console.log("Valid")
-                }
-                break;
-            case "zipCode":
-                console.log(`this is: ${name}`);
-                let zip = element.value;
-                if (zip.match("[a-zA-z+]")) {
-                    console.log("not a number")
-                } else {
-                    let zipNumber = parseInt(zip)
-                    console.log(typeof zipNumber);
-                }
-               
-                break;
-            case "password":
-                console.log(`this is: ${name}`);
-                break;
-            case "confirmPassowrd":
-                console.log(`this is: ${name}`);
-                break;
-        
-            default:
-                console.log("def")
-                break;
-        }
-       
     });
 });
 
 
+function validationSwitch(element) {
+    let name = element.name;
+    let validityState = element.validity;
+    switch (name) {
+        case "email":
+            console.log(`this is: ${name}`);
+            if (validityState.valid) {
+                console.log("Valid");
+            } else {
+                if (validityState.valueMissing) {
+                    console.log("missing")
+                } else if (validityState.typeMismatch) {
+                    console.log("not an email")
+                } else {
+                    console.log("unkown error")
+                }
+                
+            }
 
+            break;
+        case "country":
+            console.log(`this is: ${name}`);
 
-// function customValidator(target) {
-//     validityState = target.validity;
-//     labelText = target.labels[0].textContent;
-//     var errorMsg = ""
-//     switch(true){
-//         case validityState.typeMismatch:
-//             errorMsg = `you must enter: ${labelText}`;
+            if(validityState.valueMissing) {
+                console.log("missing");
+            } else if (!validCountries.includes(element.value.toUpperCase())){
+                console.log("not a country")
+            } else {
+                console.log("Valid")
+            }
+            break;
+        case "zipCode":
+            console.log(`this is: ${name}`);
+            let zip = element.value;
+            if (zip.match("[a-zA-z+]")) {
+                console.log("not a number")
+            } else {
+                let zipNumber = parseInt(zip)
+                console.log(typeof zipNumber);
+            }
             
-//             break;
-//         case validityState.valueMissing:
-//             errorMsg = 'cannot be empty'
-//             break;
-//         case validityState.tooShort:
-//             errorMsg = "too short";
-//             break
-//         default:
-//             errorMsg = "";
-//             break;
-//     }
-
-//     return errorMsg;
-// }
+            break;
+        case "password":
+            console.log(`this is: ${name}`);
+            break;
+        case "confirmPassowrd":
+            console.log(`this is: ${name}`);
+            console.log(element.previousElementSibling.previousElementSibling)
+            if (!(element.value === element.previousElementSibling.previousElementSibling.value)) {
+                console.log("don't match")
+                element.setCustomValidity("Not valid");
+                element.reportValidity();
+            } else {
+                console.log("match")
+            }
+            break;
+    
+        default:
+            console.log("def")
+            break;
+    }
+}
